c371148eb9a0ce64b20a0c6ea6e5766aa1738002
fhc02hpg <fhc02hpg@users.noreply.github.com>
2017-01-14 08:51:58 +0100

    simple recursion exercises used in lv

---
+++ src/org/campus02/pr1/lv/RecursiveMaths.java
@@ -0,0 +1,26 @@
+package org.campus02.pr1.lv;
+
+public class RecursiveMaths {
+	
+	public static int sum(int n) {
+		if(n == 0) {
+			return 0;
+		}
+		return n + sum(n-1);
+	}
+	
+	public static int fibonacci(int n) {
+		if(n <= 2) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+	
+	public static long factorial(int n) {
+		if(n <= 1) {
+			return 1;
+		}
+		return n * factorial(n-1);
+	}
+	
+}
