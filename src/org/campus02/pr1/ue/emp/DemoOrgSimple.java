775295505915e40c3a00fde37c4b8d81e81ea489
fhc02hpg <fhc02hpg@users.noreply.github.com>
2017-01-20 15:35:10 +0100

    added recursive methods for top-down walking the organigram with/out indent

--- src/org/campus02/pr1/ue/emp/DemoOrgSimple.java
+++ src/org/campus02/pr1/ue/emp/DemoOrgSimple.java
@@ -7,22 +7,27 @@ public class DemoOrgSimple {
 		EmployeeManager em = new EmployeeManager();
 		
 		//create employees and build hierarchy
-		//according to organigram
-		Employee ceo = new Employee(1, "Mitarbeiter 1", 7_500, "Management", "CEO");
+		//according to organigram in the exercise
+		Employee ceo = new Employee(1, "Mitarbeiter 1", 7_500,
+									"Management", "CEO");
 		
-		Employee cto = new Employee(2, "Mitarbeiter 2", 6_500, "Management", "CTO");
+		Employee cto = new Employee(2, "Mitarbeiter 2", 6_500,
+									"Management", "CTO");
 		cto.setSuperior(ceo);
 		ceo.addSubordinate(cto);
 		
-		Employee dev1 = new Employee(3, "Mitarbeiter 3", 3_500, "Engineering", "Dev");
+		Employee dev1 = new Employee(3, "Mitarbeiter 3", 3_500,
+									"Engineering", "Dev");
 		dev1.setSuperior(cto);
-		Employee dev2 = new Employee(4, "Mitarbeiter 4", 3_500, "Engineering", "Dev");
+		Employee dev2 = new Employee(4, "Mitarbeiter 4", 3_500,
+									"Engineering", "Dev");
 		dev2.setSuperior(cto);
 		
 		cto.addSubordinate(dev1);
 		cto.addSubordinate(dev2);
 		
-		Employee hr = new Employee(5, "Mitarbeiter 5", 6_500, "Management", "HR");
+		Employee hr = new Employee(5, "Mitarbeiter 5", 6_500,
+									"Management", "HR");
 		hr.setSuperior(ceo);
 		ceo.addSubordinate(hr);
 			
@@ -33,12 +38,25 @@ public class DemoOrgSimple {
 		em.addEmployee(dev1);
 		em.addEmployee(dev2);
 			
-		System.out.println(OrganigramHandler.getChainOfCommand(cto));
-	
 		//show chain of command for all employees
-		//for(Employee e : em.getAllEmployees()) {
-		//	System.out.println(OrganigramHandler.getChainOfCommand(e));
-		//}
+		//without the chain length
+		for(Employee e : em.getAllEmployees()) {
+			System.out.println(OrganigramHandler.getChainOfCommand(e));
+		}
+		
+		//show chain of command for all employees
+		//including the chain length at the end
+		for(Employee e : em.getAllEmployees()) {
+			System.out.println(OrganigramHandler.getChainOfCommand(e,0));
+		}
+		
+		//walking down the organigram without indent
+		String result = OrganigramHandler.processHierarchy(ceo);
+		System.out.println(result);
+
+		//walking down the organigram with indent
+		result = OrganigramHandler.processHierarchy(ceo,"");
+		System.out.println(result);
 
 	}
 
--- src/org/campus02/pr1/ue/emp/OrganigramHandler.java
+++ src/org/campus02/pr1/ue/emp/OrganigramHandler.java
@@ -1,17 +1,60 @@
 package org.campus02.pr1.ue.emp;
 
 public class OrganigramHandler {
-	
+
 	public static String getChainOfCommand(Employee employee) {
-		
-		String chain = employee.getName()  
-							+ " ["+employee.getRole()+ "]";
-		
-		if(employee.getSuperior() == null) {
+
+		String chain = employee.getName() + " [" + employee.getRole() + "]";
+
+		if (employee.getSuperior() == null) {
 			return chain;
 		}
-		
+
 		return chain + " -> " + getChainOfCommand(employee.getSuperior());
+
+	}
+
+	public static String getChainOfCommand(Employee employee, int length) {
+
+		String chain = employee.getName() + " [" + employee.getRole() + "]";
+
+		if (employee.getSuperior() == null) {
+			return chain + " (" + length + ")";
+		}
+
+		return chain + " -> " + getChainOfCommand(employee.getSuperior(), length + 1);
+	}
+	
+	public static String processHierarchy(Employee emp) {
+
+		if(emp.getSubordinates().isEmpty()) {
+			return " - "+emp.getRole() + " ["+emp.getName()+"]\n";
+		}
+		
+		String result = " + "+emp.getRole() + " ("+emp.getName()+")\n";
+		
+		for(Employee e : emp.getSubordinates()) {
+			result += processHierarchy(e);
+		}
+		
+		return result;
+		
+	}
+	
+	public static String processHierarchy(Employee emp, String indent) {
+
+		if (emp.getSubordinates().size() == 0) {
+			return indent + " - " + emp.getRole() + " [" + emp.getName() + "]\n";
+		}
+
+		String result = indent + " + " + emp.getRole() + " (" + emp.getName() + ")\n";
+		indent += "   ";
+
+		for (Employee e : emp.getSubordinates()) {
+			result += processHierarchy(e, indent);
+		}
+
+		return result;
 	}
 
 }
